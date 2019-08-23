7234d9cec1365bc564387b011520a15396b2847b
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-08-23 15:57:45 -0600

    Dirt caves, progress, but still not done

--- src/main/java/rys/common/ReadyYourShovels.java
+++ src/main/java/rys/common/ReadyYourShovels.java
@@ -2,7 +2,10 @@ package rys.common;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.storage.loot.conditions.LootConditionManager;
@@ -10,10 +13,10 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
+import rys.common.block.ModBlocks;
 import rys.common.init.ModEvents;
-import rys.common.init.ModFeatures;
 import rys.common.util.Reference;
+import rys.common.world.gen.feature.ModFeatures;
 import rys.common.world.storage.loot.conditions.MatchHarvestLevel;
 
 @Mod(Reference.MOD_ID)
@@ -30,8 +33,16 @@ public class ReadyYourShovels {
 	}
 	
 	public static void registerFeatures() {
-		ForgeRegistries.BIOMES.forEach((Biome biome) -> {
-			biome.addFeature(GenerationStage.Decoration.RAW_GENERATION, Biome.createDecoratedFeature(ModFeatures.cave, IFeatureConfig.NO_FEATURE_CONFIG, Placement.NOPE, IPlacementConfig.NO_PLACEMENT_CONFIG));
+		Biome.BIOMES.forEach((Biome biome) -> {
+			String name = biome.getRegistryName().toString();
+			if (!name.contains("ocean") && !name.contains("mountains")) {
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.clay_deposit.getDefaultState(), 32), Placement.COUNT_RANGE, new CountRangeConfig(32, 32, 0, 64)));
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.peat_deposit.getDefaultState(), 17), Placement.COUNT_RANGE, new CountRangeConfig(20, 32, 0, 128)));
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.iron_deposit.getDefaultState(), 9), Placement.COUNT_RANGE, new CountRangeConfig(20, 32, 0, 64)));
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.gold_deposit.getDefaultState(), 9), Placement.COUNT_RANGE, new CountRangeConfig(2, 32, 0, 64)));
+				
+				biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Biome.createDecoratedFeature(ModFeatures.cave, IFeatureConfig.NO_FEATURE_CONFIG, Placement.NOPE, IPlacementConfig.NO_PLACEMENT_CONFIG));
+			}
 		});
 	}
 	
--- src/main/java/rys/common/world/gen/feature/CaveFeature.java
+++ src/main/java/rys/common/world/gen/feature/CaveFeature.java
@@ -15,52 +15,71 @@ import net.minecraft.world.gen.GenerationSettings;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
-import rys.common.init.ModBlocks;
-import rys.common.util.EntryList;
+import rys.common.block.ModBlocks;
 
 public class CaveFeature extends Feature<NoFeatureConfig> {
-	
-	private EntryList<Block> blockList = new EntryList<Block>()
-			.addEntry(ModBlocks.tough_dirt, 120)
-			.addEntry(ModBlocks.regolith, 57)
-			.addEntry(ModBlocks.clay_deposit, 26)
-			.addEntry(ModBlocks.peat_deposit, 11)
-			.addEntry(ModBlocks.iron_deposit, 4)
-			.addEntry(ModBlocks.gold_deposit, 1);
 	
 	public CaveFeature(Function<Dynamic<?>, ? extends NoFeatureConfig> p_i49914_1_) {
 		super(p_i49914_1_);
 	}
 	
 	public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-		int j = worldIn.getHeight(Heightmap.Type.WORLD_SURFACE_WG, pos.getX(), pos.getZ());
 		
-		for (int x = pos.getX(); x < pos.getX() + 16; x++) {
-			for (int z = pos.getZ(); z < pos.getZ() + 16; z++) {
-				for (int y = (j - 16); y < (j + 16); y++) {
-					this.tryPlace(worldIn, new BlockPos(x, y, z));
+		for (int x = 0; x < 16; x++) {
+			for (int z = 0; z < 16; z++) {
+				
+				int surfaceY = worldIn.getHeight(Heightmap.Type.WORLD_SURFACE_WG, pos.getX(), pos.getZ());
+				
+				for (int y = (surfaceY - 32); y < (surfaceY + 32); y++) {
+					this.tryPlace(worldIn, new BlockPos(x + pos.getX(), y, z + pos.getZ()));
 				}
+				
+				this.tryPlaceLayer(worldIn, rand, new BlockPos(x + pos.getX(), surfaceY - 32, z + pos.getZ()));
+				
 			}
 		}
 		
 		return true;
 	}
 	
-	private void tryPlace(IWorld worldIn, BlockPos pos) {
-		BlockState state = worldIn.getBlockState(pos);
+	private void tryPlace(IWorld world, BlockPos pos) {
+		BlockState state = world.getBlockState(pos);
+		BlockPos[] arrayOfPos = new BlockPos[] { new BlockPos(-1, -1, -1), new BlockPos(-1, -1, 0), new BlockPos(-1, -1, 1), new BlockPos(0, -1, -1), new BlockPos(0, -1, 0), new BlockPos(0, -1, 1), new BlockPos(1, -1, -1), new BlockPos(1, -1, 0), new BlockPos(1, -1, 1), new BlockPos(-1, 0, -1), new BlockPos(-1, 0, 0), new BlockPos(-1, 0, 1), new BlockPos(0, 0, -1), new BlockPos(0, 0, 1), new BlockPos(1, 0, -1), new BlockPos(1, 0, 0), new BlockPos(1, 0, 1), new BlockPos(-1, 1, -1), new BlockPos(-1, 1, 0), new BlockPos(-1, 1, 1), new BlockPos(0, 1, -1), new BlockPos(0, 1, 0), new BlockPos(0, 1, 1), new BlockPos(1, 1, -1), new BlockPos(1, 1, 0), new BlockPos(1, 1, 1) };
 		
-		if (this.isStone(state)) {
-			this.setBlockState(worldIn, pos, this.getRandomBlock());
+		for (BlockPos posInArray : arrayOfPos) {
+			if (world.isAirBlock(pos.add(posInArray))) {
+				if (this.isStone(state)) {
+					this.setBlockState(world, pos, ModBlocks.tough_dirt.getDefaultState());
+				}
+				if (state.getBlock() == Blocks.COBBLESTONE) {
+					this.setBlockState(world, pos, ModBlocks.regolith.getDefaultState());
+				}
+				if (state.getBlock() == Blocks.COAL_ORE) {
+					this.setBlockState(world, pos, ModBlocks.peat_deposit.getDefaultState());
+				}
+				if (state.getBlock() == Blocks.IRON_ORE) {
+					this.setBlockState(world, pos, ModBlocks.iron_deposit.getDefaultState());
+				}
+				if (state.getBlock() == Blocks.GOLD_ORE) {
+					this.setBlockState(world, pos, ModBlocks.gold_deposit.getDefaultState());
+				}
+			}
 		}
 	}
 	
-	private boolean isStone(BlockState stateIn) {
-		Block block = stateIn.getBlock();
-		return block == Blocks.COBBLESTONE || block == Blocks.STONE || block == Blocks.GRANITE || block == Blocks.DIORITE || block == Blocks.ANDESITE;
+	private void tryPlaceLayer(IWorld world, Random random, BlockPos pos) {
+		BlockState state = world.getBlockState(pos);
+		
+		if (state.getBlock() == ModBlocks.tough_dirt) {
+			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2)), 0), ModBlocks.regolith.getDefaultState());
+			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2) + 1), 0), ModBlocks.regolith.getDefaultState());
+			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2) + 2), 0), ModBlocks.regolith.getDefaultState());
+		}
 	}
 	
-	private BlockState getRandomBlock() {
-		return this.blockList.getRandom().getDefaultState();
+	private boolean isStone(BlockState state) {
+		Block block = state.getBlock();
+		return block == Blocks.STONE || block == Blocks.GRANITE || block == Blocks.DIORITE || block == Blocks.ANDESITE;
 	}
 	
 }
