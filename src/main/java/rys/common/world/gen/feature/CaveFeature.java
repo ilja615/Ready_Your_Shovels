4a625d9ae3e6f6ffb602dbfddda3951442ca00ee
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-08-30 23:03:45 -0600

    Cave feature improved

--- src/main/java/rys/common/world/gen/feature/CaveFeature.java
+++ src/main/java/rys/common/world/gen/feature/CaveFeature.java
@@ -5,37 +5,42 @@ import java.util.function.Function;
 
 import com.mojang.datafixers.Dynamic;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationSettings;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraftforge.common.Tags;
 import rys.common.block.ModBlocks;
 
 public class CaveFeature extends Feature<NoFeatureConfig> {
 	
-	public CaveFeature(Function<Dynamic<?>, ? extends NoFeatureConfig> p_i49914_1_) {
-		super(p_i49914_1_);
+	private static BlockPos[] arrayOfPos = new BlockPos[] { new BlockPos(-1, -1, -1), new BlockPos(-1, -1, 0), new BlockPos(-1, -1, 1), new BlockPos(0, -1, -1), new BlockPos(0, -1, 0), new BlockPos(0, -1, 1), new BlockPos(1, -1, -1), new BlockPos(1, -1, 0), new BlockPos(1, -1, 1), new BlockPos(-1, 0, -1), new BlockPos(-1, 0, 0), new BlockPos(-1, 0, 1), new BlockPos(0, 0, -1), new BlockPos(0, 0, 1), new BlockPos(1, 0, -1), new BlockPos(1, 0, 0), new BlockPos(1, 0, 1), new BlockPos(-1, 1, -1), new BlockPos(-1, 1, 0), new BlockPos(-1, 1, 1), new BlockPos(0, 1, -1), new BlockPos(0, 1, 0), new BlockPos(0, 1, 1), new BlockPos(1, 1, -1), new BlockPos(1, 1, 0), new BlockPos(1, 1, 1) };
+	
+	public CaveFeature(Function<Dynamic<?>, ? extends NoFeatureConfig> configFactoryIn) {
+		super(configFactoryIn);
 	}
 	
 	public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-		
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
+				int surfaceY = worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING, pos.add(x, 0, z)).getY();
 				
-				int surfaceY = worldIn.getHeight(Heightmap.Type.WORLD_SURFACE_WG, pos.getX(), pos.getZ());
+				Biome biome = worldIn.getBiome(pos.add(x, 0, z));
+				String name = biome.getRegistryName().toString();
 				
-				for (int y = (surfaceY - 32); y < (surfaceY + 32); y++) {
-					this.tryPlace(worldIn, new BlockPos(x + pos.getX(), y, z + pos.getZ()));
+				if (!name.contains("ocean") && !name.contains("mountains") && !name.contains("river") && !name.contains("stone")) {
+					for (int y = surfaceY - 32; y < surfaceY; y++) {
+						this.tryPlace(worldIn, pos.add(x, y, z));
+					}
+					
+					this.tryPlaceLayer(worldIn, rand, pos.add(x, surfaceY - 32, z));
 				}
-				
-				this.tryPlaceLayer(worldIn, rand, new BlockPos(x + pos.getX(), surfaceY - 32, z + pos.getZ()));
-				
 			}
 		}
 		
@@ -44,42 +49,52 @@ public class CaveFeature extends Feature<NoFeatureConfig> {
 	
 	private void tryPlace(IWorld world, BlockPos pos) {
 		BlockState state = world.getBlockState(pos);
-		BlockPos[] arrayOfPos = new BlockPos[] { new BlockPos(-1, -1, -1), new BlockPos(-1, -1, 0), new BlockPos(-1, -1, 1), new BlockPos(0, -1, -1), new BlockPos(0, -1, 0), new BlockPos(0, -1, 1), new BlockPos(1, -1, -1), new BlockPos(1, -1, 0), new BlockPos(1, -1, 1), new BlockPos(-1, 0, -1), new BlockPos(-1, 0, 0), new BlockPos(-1, 0, 1), new BlockPos(0, 0, -1), new BlockPos(0, 0, 1), new BlockPos(1, 0, -1), new BlockPos(1, 0, 0), new BlockPos(1, 0, 1), new BlockPos(-1, 1, -1), new BlockPos(-1, 1, 0), new BlockPos(-1, 1, 1), new BlockPos(0, 1, -1), new BlockPos(0, 1, 0), new BlockPos(0, 1, 1), new BlockPos(1, 1, -1), new BlockPos(1, 1, 0), new BlockPos(1, 1, 1) };
 		
-		for (BlockPos posInArray : arrayOfPos) {
-			if (world.isAirBlock(pos.add(posInArray))) {
-				if (this.isStone(state)) {
-					this.setBlockState(world, pos, ModBlocks.tough_dirt.getDefaultState());
-				}
-				if (state.getBlock() == Blocks.COBBLESTONE) {
-					this.setBlockState(world, pos, ModBlocks.regolith.getDefaultState());
-				}
-				if (state.getBlock() == Blocks.COAL_ORE) {
-					this.setBlockState(world, pos, ModBlocks.peat_deposit.getDefaultState());
-				}
-				if (state.getBlock() == Blocks.IRON_ORE) {
-					this.setBlockState(world, pos, ModBlocks.iron_deposit.getDefaultState());
-				}
-				if (state.getBlock() == Blocks.GOLD_ORE) {
-					this.setBlockState(world, pos, ModBlocks.gold_deposit.getDefaultState());
-				}
-			}
+		if (state.isIn(Tags.Blocks.STONE)) {
+			this.trySetBlockState(world, pos, ModBlocks.tough_dirt.getDefaultState());
+		}
+		
+		if (state.getBlock() == Blocks.COAL_ORE) {
+			this.trySetBlockState(world, pos, ModBlocks.peat_deposit.getDefaultState());
+		}
+		
+		if (state.getBlock() == Blocks.IRON_ORE) {
+			this.trySetBlockState(world, pos, ModBlocks.iron_deposit.getDefaultState());
+		}
+		
+		if (state.getBlock() == Blocks.GOLD_ORE) {
+			this.trySetBlockState(world, pos, ModBlocks.gold_deposit.getDefaultState());
 		}
 	}
 	
 	private void tryPlaceLayer(IWorld world, Random random, BlockPos pos) {
-		BlockState state = world.getBlockState(pos);
+		BlockPos pos_0 = pos.add(0, random.nextInt(2) - random.nextInt(2), 0);
+		BlockPos pos_1 = pos.add(0, (random.nextInt(2) - random.nextInt(2)) - 1, 0);
+		BlockPos pos_2 = pos.add(0, (random.nextInt(2) - random.nextInt(2)) - 2, 0);
 		
-		if (state.getBlock() == ModBlocks.tough_dirt) {
-			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2)), 0), ModBlocks.regolith.getDefaultState());
-			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2) + 1), 0), ModBlocks.regolith.getDefaultState());
-			this.setBlockState(world, pos.add(0, (random.nextInt(2) - random.nextInt(2) + 2), 0), ModBlocks.regolith.getDefaultState());
+		BlockState state_0 = world.getBlockState(pos_0);
+		BlockState state_1 = world.getBlockState(pos_1);
+		BlockState state_2 = world.getBlockState(pos_2);
+		
+		if (state_0.isIn(Tags.Blocks.STONE) || state_0.getBlock() == ModBlocks.tough_dirt) {
+			this.trySetBlockState(world, pos_0, ModBlocks.regolith.getDefaultState());
+		}
+		
+		if (state_1.isIn(Tags.Blocks.STONE) || state_1.getBlock() == ModBlocks.tough_dirt) {
+			this.trySetBlockState(world, pos_1, ModBlocks.regolith.getDefaultState());
+		}
+		
+		if (state_2.isIn(Tags.Blocks.STONE) || state_2.getBlock() == ModBlocks.tough_dirt) {
+			this.trySetBlockState(world, pos_2, ModBlocks.regolith.getDefaultState());
 		}
 	}
 	
-	private boolean isStone(BlockState state) {
-		Block block = state.getBlock();
-		return block == Blocks.STONE || block == Blocks.GRANITE || block == Blocks.DIORITE || block == Blocks.ANDESITE;
+	private void trySetBlockState(IWorld world, BlockPos pos, BlockState state) {
+		for (BlockPos posInArray : arrayOfPos) {
+			if (world.isAirBlock(pos.add(posInArray))) {
+				this.setBlockState(world, pos, state);
+			}
+		}
 	}
 	
 }
