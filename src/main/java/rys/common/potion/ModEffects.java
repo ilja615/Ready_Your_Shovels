fa14a4bd1cee22a5c5a1b5c3d3884a8a29475cba
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-09-06 21:29:10 -0600

    Update ModEffects.java
    Now using @ObjectHolder

--- src/main/java/rys/common/potion/ModEffects.java
+++ src/main/java/rys/common/potion/ModEffects.java
@@ -6,15 +6,17 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.ObjectHolder;
 import rys.common.util.Reference;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@ObjectHolder(Reference.MOD_ID)
 public class ModEffects {
 	
-	public static final Effect decrease_debuff = new EffectMod("decrease_debuff", EffectType.BENEFICIAL, 16544256);
-	public static final Effect increase_buff = new EffectMod("increase_buff", EffectType.BENEFICIAL, 16756506);
-	public static final Effect increase_debuff = new EffectMod("increase_debuff", EffectType.HARMFUL, 13340727);
-	public static final Effect decrease_buff = new EffectMod("decrease_buff", EffectType.HARMFUL, 15976297);
+	public static final EffectMod decrease_debuff = create("decrease_debuff", new EffectMod(EffectType.BENEFICIAL, 16544256));
+	public static final EffectMod increase_buff = create("increase_buff", new EffectMod(EffectType.BENEFICIAL, 16756506));
+	public static final EffectMod increase_debuff = create("increase_debuff", new EffectMod(EffectType.HARMFUL, 13340727));
+	public static final EffectMod decrease_buff = create("decrease_buff", new EffectMod(EffectType.HARMFUL, 15976297));
 	
 	@SubscribeEvent
 	public static void registerEffects(RegistryEvent.Register<Effect> event) {
@@ -24,6 +26,11 @@ public class ModEffects {
 		registry.register(increase_buff);
 		registry.register(increase_debuff);
 		registry.register(decrease_buff);
+	}
+	
+	public static <T extends Effect> T create(String name, T effect) {
+		effect.setRegistryName(Reference.MOD_ID, name);
+		return effect;
 	}
 	
 }
