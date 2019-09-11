8ee9e864f49b46413ba63a4e31aa694a98d1201c
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-09-10 19:15:04 -0600

    Update ModBlockProperties.java
    Dayroot light level changed from 15 to 7

--- src/main/java/rys/common/block/ModBlockProperties.java
+++ src/main/java/rys/common/block/ModBlockProperties.java
@@ -17,6 +17,6 @@ public class ModBlockProperties {
 	public static final Block.Properties gold_deposit = Block.Properties.create(Material.EARTH).hardnessAndResistance(3.0F, 0.5F).sound(SoundType.GROUND).harvestLevel(3).harvestTool(ToolType.SHOVEL);
 	public static final Block.Properties planter_box = Block.Properties.create(Material.WOOD).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD).harvestLevel(0).harvestTool(ToolType.AXE);
 	public static final Block.Properties fruit_tree = Block.Properties.create(Material.PLANTS).hardnessAndResistance(2.0F, 2.0F).sound(SoundType.PLANT).harvestLevel(0).harvestTool(ToolType.AXE).doesNotBlockMovement().tickRandomly();
-	public static final Block.Properties dayroot = Block.Properties.create(Material.TALL_PLANTS).hardnessAndResistance(0.0F, 0.0F).sound(SoundType.PLANT).harvestLevel(0).doesNotBlockMovement().lightValue(15);
+	public static final Block.Properties dayroot = Block.Properties.create(Material.TALL_PLANTS).hardnessAndResistance(0.0F, 0.0F).sound(SoundType.PLANT).harvestLevel(0).doesNotBlockMovement().lightValue(7);
 	
 }
