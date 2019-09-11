6f65d94dab6fd87f2721558a839f7bd018437902
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-09-10 19:20:43 -0600

    Effects and potions not done
    Issue: status effect time doesn't update when changed. Re-do the potion recipes

--- src/main/java/rys/common/potion/EffectMod.java
+++ src/main/java/rys/common/potion/EffectMod.java
@@ -1,8 +1,8 @@
 package rys.common.potion;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
@@ -14,69 +14,41 @@ public class EffectMod extends Effect {
 		super(typeIn, colorIn);
 	}
 	
-	public void affectEntity(Entity source, Entity indirectSource, LivingEntity entity, int amplifier, double health) {
+	public void performEffect(LivingEntity entity, int amplifier) {
+		Map<Effect, EffectInstance> map = new HashMap<>();
+		
 		int time = (amplifier + 1) * 200;
 		
-		for (Map.Entry<Effect, EffectInstance> entry : entity.getActivePotionMap().entrySet()) {
-			Effect effect = entry.getKey();
-			EffectInstance effectInstance = entry.getValue();
-			
-			if (effect != this && !effectInstance.isAmbient()) {
-				
+		if (this == ModEffects.decrease_debuff) {
+			entity.getActivePotionMap().forEach((Effect effect, EffectInstance effectInstance) -> {
 				if (effect.getEffectType() == EffectType.HARMFUL) {
-					
-					if (this == ModEffects.decrease_debuff) {
-						entity.getActivePotionMap().remove(effect);
-						entity.livingTick();
-						entity.getActivePotionMap().put(effect, new EffectInstance(effect, effectInstance.getDuration() - time, effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.isShowIcon()));
-						break;
-					}
-					
-					if (this == ModEffects.increase_debuff) {
-						entity.getActivePotionMap().remove(effect);
-						entity.livingTick();
-						entity.getActivePotionMap().put(effect, new EffectInstance(effect, effectInstance.getDuration() + time, effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.isShowIcon()));
-						break;
-					}
-					
+					map.put(effect, new EffectInstance(effect, effectInstance.getDuration() - time, effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.isShowIcon()));
+				} else {
+					map.put(effect, effectInstance);
 				}
-				
-				if (effect.getEffectType() == EffectType.BENEFICIAL) {
-					
-					if (this == ModEffects.increase_buff) {
-						entity.getActivePotionMap().remove(effect);
-						entity.livingTick();
-						entity.getActivePotionMap().put(effect, new EffectInstance(effect, effectInstance.getDuration() + time, effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.isShowIcon()));
-						break;
-					}
-					
-					if (this == ModEffects.decrease_buff) {
-						entity.getActivePotionMap().remove(effect);
-						entity.livingTick();
-						entity.getActivePotionMap().put(effect, new EffectInstance(effect, effectInstance.getDuration() - time, effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.isShowIcon()));
-						break;
-					}
-					
-				}
-				
-			}
+			});
+			
+			entity.getActivePotionMap().clear();
+			
+			map.forEach((Effect effect, EffectInstance effectInstance) -> {
+				entity.getActivePotionMap().put(effect, effectInstance);
+			});
 		}
-	}
-	
-	public boolean isInstant() {
-		if (this == ModEffects.decrease_debuff || this == ModEffects.increase_buff || this == ModEffects.increase_debuff || this == ModEffects.increase_debuff) {
-			return true;
-		}
-		
-		return super.isInstant();
 	}
 	
 	public boolean isReady(int duration, int amplifier) {
-		if (this == ModEffects.decrease_debuff || this == ModEffects.increase_buff || this == ModEffects.increase_debuff || this == ModEffects.increase_debuff) {
-			return duration >= 1;
+		if (this == ModEffects.decrease_debuff || this == ModEffects.increase_buff || this == ModEffects.increase_debuff || this == ModEffects.decrease_buff) {
+			return true;
 		}
 		
-		return super.isReady(duration, amplifier);
+		return false;
 	}
 	
+	public boolean isInstant() {
+		if (this == ModEffects.decrease_debuff || this == ModEffects.increase_buff || this == ModEffects.increase_debuff || this == ModEffects.decrease_buff) {
+			return true;
+		}
+		
+		return false;
+	}
 }
