ae4d607679c94eaa45e7bde40d19c63c1540dfbb
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-09-16 15:55:38 -0600

    Update PlanterBoxBlock.java
    Now extends from FarmlandBlock so villagers can replant on them

--- src/main/java/rys/common/block/PlanterBoxBlock.java
+++ src/main/java/rys/common/block/PlanterBoxBlock.java
@@ -2,26 +2,64 @@ package rys.common.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.PlantType;
 
-public class PlanterBoxBlock extends Block {
+public class PlanterBoxBlock extends FarmlandBlock {
 	
 	public PlanterBoxBlock(Block.Properties properties) {
 		super(properties);
+		this.setDefaultState(this.stateContainer.getBaseState().with(BlockStateProperties.MOISTURE_0_7, Integer.valueOf(0)));
 	}
 	
 	public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction facing, IPlantable plantable) {
-		PlantType type = plantable.getPlantType(world, pos.offset(facing));
-		
-		if (type == PlantType.Crop || type == PlantType.Plains || type == PlantType.Desert || type == PlantType.Cave) {
-			return true;
+		if (facing == Direction.UP) {
+			PlantType type = plantable.getPlantType(world, pos.offset(facing));
+			
+			if (type == PlantType.Crop || type == PlantType.Plains || type == PlantType.Desert || type == PlantType.Cave) {
+				return true;
+			}
+			
 		}
 		
 		return false;
+	}
+	
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+		return stateIn;
+	}
+	
+	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		return true;
+	}
+	
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return this.getDefaultState();
+	}
+	
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return VoxelShapes.fullCube();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance) {
+		entityIn.fall(fallDistance, 1.0F);
 	}
 	
 }
