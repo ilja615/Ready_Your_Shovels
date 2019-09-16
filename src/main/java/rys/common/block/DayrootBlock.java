57b1d5566d0ef179f6970c06001311fee0bf5f5c
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2019-09-16 15:56:47 -0600

    Update DayrootBlock.java
    Removed unnecessary comments, and added a farming option

--- src/main/java/rys/common/block/DayrootBlock.java
+++ src/main/java/rys/common/block/DayrootBlock.java
@@ -1,9 +1,12 @@
 package rys.common.block;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BushBlock;
+import net.minecraft.block.IGrowable;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
@@ -15,11 +18,12 @@ import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 
-public class DayrootBlock extends BushBlock {
+public class DayrootBlock extends BushBlock implements IGrowable {
 	
 	public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
 	
@@ -28,54 +32,47 @@ public class DayrootBlock extends BushBlock {
 		this.setDefaultState(this.stateContainer.getBaseState().with(HALF, DoubleBlockHalf.UPPER));
 	}
 	
-	// BlockState updateShape(BlockState, Direction, BlockState, LevelAccessor, BlockPos, BlockPos)
 	public BlockState updatePostPlacement(BlockState state, Direction direction, BlockState directionState, IWorld world, BlockPos currentPos, BlockPos directionPos) {
 		DoubleBlockHalf half = state.get(HALF);
 		
-		if ((half == DoubleBlockHalf.UPPER && direction == Direction.DOWN && directionState.getBlock() == this && directionState.get(HALF) != half) || (half == DoubleBlockHalf.LOWER && direction == Direction.UP && directionState.getBlock() == this && directionState.get(HALF) != half) || (half == DoubleBlockHalf.LOWER && direction == Direction.DOWN) || (direction.getAxis() != Direction.Axis.Y)) {
+		if ((direction.getAxis() != Direction.Axis.Y) || (half == DoubleBlockHalf.UPPER && direction == Direction.DOWN && directionState.getBlock() == this && directionState.get(HALF) != half) || (half == DoubleBlockHalf.LOWER && direction == Direction.UP && directionState.getBlock() == this && directionState.get(HALF) != half) || (half == DoubleBlockHalf.LOWER && direction == Direction.DOWN)) {
 			return super.updatePostPlacement(state, direction, directionState, world, currentPos, directionPos);
 		} else {
 			return Blocks.AIR.getDefaultState();
 		}
 	}
 	
-	// BlockState getStateForPlacement(BlockPlaceContext)
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
 		if (context.getWorld().getBlockState(context.getPos().down()).isReplaceable(context)) {
 			return super.getStateForPlacement(context);
 		} else {
-			return Blocks.AIR.getDefaultState();
+			return null;
 		}
 	}
 	
-	// void setPlacedBy(Level, BlockPos, BlockState, LivingEntity, ItemStack)
 	public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
 		world.setBlockState(pos.down(), this.getDefaultState().with(HALF, DoubleBlockHalf.LOWER), 3);
 	}
 	
-	// boolean canSurvive(BlockState, LevelReader, BlockPos)
 	public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos pos) {
 		BlockState state_1 = world.getBlockState(pos.up());
 		
 		if (state.get(HALF) == DoubleBlockHalf.UPPER) {
-			return state_1.canSustainPlant(world, pos.up(), Direction.DOWN, this);
+			return state_1.canSustainPlant(world, pos.up(), Direction.DOWN, this) || Block.isRock(state_1.getBlock());
 		} else {
 			return state_1.getBlock() == this && state_1.get(HALF) == DoubleBlockHalf.UPPER;
 		}
 	}
 	
-	// void placeAt(LevelAccessor, BlockPos, int)
 	public void placeAt(IWorld world, BlockPos pos, int flags) {
 		world.setBlockState(pos, this.getDefaultState().with(HALF, DoubleBlockHalf.UPPER), flags);
 		world.setBlockState(pos.down(), this.getDefaultState().with(HALF, DoubleBlockHalf.LOWER), flags);
 	}
 	
-	// void playerDestroy(Level, Player, BlockPos, BlockState, BlockEntity, ItemStack)
 	public void harvestBlock(World world, PlayerEntity player, BlockPos pos, BlockState state, TileEntity te, ItemStack stack) {
 		super.harvestBlock(world, player, pos, Blocks.AIR.getDefaultState(), te, stack);
 	}
 	
-	// void playerWillDestroy(Level, BlockPos, BlockState, Player)
 	public void onBlockHarvested(World world, BlockPos pos, BlockState state, PlayerEntity player) {
 		BlockPos pos_1 = pos.up();
 		
@@ -93,9 +90,32 @@ public class DayrootBlock extends BushBlock {
 		super.onBlockHarvested(world, pos_1, state, player);
 	}
 	
-	// void createBlockStateDefinition(StateDefinition.Builder)
 	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
 		builder.add(HALF);
+	}
+	
+	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+		return true;
+	}
+	
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+		return true;
+	}
+	
+	public void grow(World worldIn, Random rand, BlockPos pos, BlockState state) {
+		for (int n = 0; n < 16; n++) {
+			int x = rand.nextInt(9) - 4;
+			int z = rand.nextInt(9) - 4;
+			int y = rand.nextInt(5) - 2;
+			
+			BlockPos pos_n = pos.add(x, y, z);
+			
+			if (rand.nextFloat() < 0.5F) {
+				if (worldIn.isAirBlock(pos_n) && worldIn.isAirBlock(pos_n.down()) && state.isValidPosition(worldIn, pos_n)) {
+					this.placeAt(worldIn, pos_n, 3);
+				}
+			}
+		}
 	}
 	
 }
