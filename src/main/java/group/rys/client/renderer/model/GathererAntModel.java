b0138318b67f478972db77586414e81a1a6acd3f
Axoladdy <58278820+Axoladdy@users.noreply.github.com>
2019-11-27 20:42:38 -0500

    Update GathererAntModel.java

--- src/main/java/group/rys/client/renderer/model/GathererAntModel.java
+++ src/main/java/group/rys/client/renderer/model/GathererAntModel.java
@@ -1,179 +1,170 @@
-package net.minecraft.world.gen.carver;
+package group.rys.client.renderer.model;
 
-import com.google.common.collect.ImmutableSet;
-import com.mojang.datafixers.Dynamic;
-import java.util.BitSet;
-import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Function;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.fluid.IFluidState;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
+import group.rys.common.entity.GathererAntEntity;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.client.renderer.model.ModelBox;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.chunk.IChunk;
-import net.minecraft.world.gen.feature.ProbabilityConfig;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public abstract class WorldCarver<C extends ICarverConfig> extends net.minecraftforge.registries.ForgeRegistryEntry<WorldCarver<?>> {
-   public static final WorldCarver<ProbabilityConfig> CAVE = register("cave", new CaveWorldCarver(ProbabilityConfig::deserialize, 256));
-   public static final WorldCarver<ProbabilityConfig> HELL_CAVE = register("hell_cave", new NetherCaveWorldCarver(ProbabilityConfig::deserialize));
-   public static final WorldCarver<ProbabilityConfig> CANYON = register("canyon", new CanyonWorldCarver(ProbabilityConfig::deserialize));
-   public static final WorldCarver<ProbabilityConfig> UNDERWATER_CANYON = register("underwater_canyon", new UnderwaterCanyonWorldCarver(ProbabilityConfig::deserialize));
-   public static final WorldCarver<ProbabilityConfig> UNDERWATER_CAVE = register("underwater_cave", new UnderwaterCaveWorldCarver(ProbabilityConfig::deserialize));
-   protected static final BlockState AIR = Blocks.AIR.getDefaultState();
-   protected static final BlockState CAVE_AIR = Blocks.CAVE_AIR.getDefaultState();
-   protected static final IFluidState WATER = Fluids.WATER.getDefaultState();
-   protected static final IFluidState LAVA = Fluids.LAVA.getDefaultState();
-   protected Set<Block> carvableBlocks = ImmutableSet.of(Blocks.STONE, Blocks.GRANITE, Blocks.DIORITE, Blocks.ANDESITE, Blocks.DIRT, Blocks.COARSE_DIRT, Blocks.PODZOL, Blocks.GRASS_BLOCK, Blocks.TERRACOTTA, Blocks.WHITE_TERRACOTTA, Blocks.ORANGE_TERRACOTTA, Blocks.MAGENTA_TERRACOTTA, Blocks.LIGHT_BLUE_TERRACOTTA, Blocks.YELLOW_TERRACOTTA, Blocks.LIME_TERRACOTTA, Blocks.PINK_TERRACOTTA, Blocks.GRAY_TERRACOTTA, Blocks.LIGHT_GRAY_TERRACOTTA, Blocks.CYAN_TERRACOTTA, Blocks.PURPLE_TERRACOTTA, Blocks.BLUE_TERRACOTTA, Blocks.BROWN_TERRACOTTA, Blocks.GREEN_TERRACOTTA, Blocks.RED_TERRACOTTA, Blocks.BLACK_TERRACOTTA, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.MYCELIUM, Blocks.SNOW, Blocks.PACKED_ICE);
-   protected Set<Fluid> carvableFluids = ImmutableSet.of(Fluids.WATER);
-   private final Function<Dynamic<?>, ? extends C> field_222721_m;
-   protected final int maxHeight;
+@OnlyIn(Dist.CLIENT)
+public class GathererAntModel extends EntityModel<GathererAntEntity> {
+	private final RendererModel body;
+	private final RendererModel head;
+	private final RendererModel antennae;
+	private final RendererModel thorax;
+	private final RendererModel abdomen;
+	private final RendererModel legs;
+	private final RendererModel left_leg_set;
+	private final RendererModel left_leg_1;
+	private final RendererModel left_leg_2;
+	private final RendererModel left_leg_3;
+	private final RendererModel right_leg_set;
+	private final RendererModel right_leg_1;
+	private final RendererModel right_leg_2;
+	private final RendererModel right_leg_3;
+	private GathererAntEntity entityIn;
+//	private final RendererModel item;
 
-   private static <C extends ICarverConfig, F extends WorldCarver<C>> F register(String key, F carver) {
-      return (F)(Registry.<WorldCarver<?>>register(Registry.CARVER, key, carver));
-   }
+	public GathererAntModel() {
+		textureWidth = 32;
+		textureHeight = 32;
 
-   public WorldCarver(Function<Dynamic<?>, ? extends C> p_i49921_1_, int p_i49921_2_) {
-      this.field_222721_m = p_i49921_1_;
-      this.maxHeight = p_i49921_2_;
-   }
+		body = new RendererModel(this);
+		body.setRotationPoint(0.0F, 24.0F, 0.0F);
 
-   public int func_222704_c() {
-      return 4;
-   }
+		head = new RendererModel(this);
+		head.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(head);
+		head.cubeList.add(new ModelBox(head, 0, 0, -2.0F, -7.0F, -4.0F, 4, 4, 4, 0.0F, false));
 
-   protected boolean func_222705_a(IChunk chunkIn, long seed, int p_222705_4_, int p_222705_5_, int p_222705_6_, double p_222705_7_, double p_222705_9_, double p_222705_11_, double p_222705_13_, double p_222705_15_, BitSet p_222705_17_) {
-      Random random = new Random(seed + (long)p_222705_5_ + (long)p_222705_6_);
-      double d0 = (double)(p_222705_5_ * 16 + 8);
-      double d1 = (double)(p_222705_6_ * 16 + 8);
-      if (!(p_222705_7_ < d0 - 16.0D - p_222705_13_ * 2.0D) && !(p_222705_11_ < d1 - 16.0D - p_222705_13_ * 2.0D) && !(p_222705_7_ > d0 + 16.0D + p_222705_13_ * 2.0D) && !(p_222705_11_ > d1 + 16.0D + p_222705_13_ * 2.0D)) {
-         int i = Math.max(MathHelper.floor(p_222705_7_ - p_222705_13_) - p_222705_5_ * 16 - 1, 0);
-         int j = Math.min(MathHelper.floor(p_222705_7_ + p_222705_13_) - p_222705_5_ * 16 + 1, 16);
-         int k = Math.max(MathHelper.floor(p_222705_9_ - p_222705_15_) - 1, 1);
-         int l = Math.min(MathHelper.floor(p_222705_9_ + p_222705_15_) + 1, this.maxHeight - 8);
-         int i1 = Math.max(MathHelper.floor(p_222705_11_ - p_222705_13_) - p_222705_6_ * 16 - 1, 0);
-         int j1 = Math.min(MathHelper.floor(p_222705_11_ + p_222705_13_) - p_222705_6_ * 16 + 1, 16);
-         if (this.func_222700_a(chunkIn, p_222705_5_, p_222705_6_, i, j, k, l, i1, j1)) {
-            return false;
-         } else {
-            boolean flag = false;
-            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-            BlockPos.MutableBlockPos blockpos$mutableblockpos1 = new BlockPos.MutableBlockPos();
-            BlockPos.MutableBlockPos blockpos$mutableblockpos2 = new BlockPos.MutableBlockPos();
+		antennae = new RendererModel(this);
+		antennae.setRotationPoint(0.0F, -6.0F, -3.0F);
+//		setRotationAngles(antennae, 0.5236F, 0.0F, 0.0F);
+		head.addChild(antennae);
+		antennae.cubeList.add(new ModelBox(antennae, 16, 13, -4.0F, -5.0F, 0.0F, 8, 4, 0, 0.0F, false));
 
-            for(int k1 = i; k1 < j; ++k1) {
-               int l1 = k1 + p_222705_5_ * 16;
-               double d2 = ((double)l1 + 0.5D - p_222705_7_) / p_222705_13_;
+		thorax = new RendererModel(this);
+		thorax.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(thorax);
+		thorax.cubeList.add(new ModelBox(thorax, 0, 8, -1.5F, -4.0F, -1.0F, 3, 2, 3, 0.0F, false));
 
-               for(int i2 = i1; i2 < j1; ++i2) {
-                  int j2 = i2 + p_222705_6_ * 16;
-                  double d3 = ((double)j2 + 0.5D - p_222705_11_) / p_222705_13_;
-                  if (!(d2 * d2 + d3 * d3 >= 1.0D)) {
-                     AtomicBoolean atomicboolean = new AtomicBoolean(false);
+		abdomen = new RendererModel(this);
+		abdomen.setRotationPoint(0.0F, -1.0F, 0.0F);
+//		setRotationAngles(abdomen, -0.1745F, 0.0F, 0.0F);
+		body.addChild(abdomen);
+		abdomen.cubeList.add(new ModelBox(abdomen, 18, 0, -2.0F, -5.0F, 1.0F, 4, 3, 3, 0.0F, false));
 
-                     for(int k2 = l; k2 > k; --k2) {
-                        double d4 = ((double)k2 - 0.5D - p_222705_9_) / p_222705_15_;
-                        if (!this.func_222708_a(d2, d4, d3, k2)) {
-                           flag |= this.carveBlock(chunkIn, p_222705_17_, random, blockpos$mutableblockpos, blockpos$mutableblockpos1, blockpos$mutableblockpos2, p_222705_4_, p_222705_5_, p_222705_6_, l1, j2, k1, k2, i2, atomicboolean);
-                        }
-                     }
-                  }
-               }
-            }
+		legs = new RendererModel(this);
+		legs.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(legs);
 
-            return flag;
-         }
-      } else {
-         return false;
-      }
-   }
+		left_leg_set = new RendererModel(this);
+		left_leg_set.setRotationPoint(0.0F, -2.0F, 0.0F);
+//		setRotationAngles(left_legs, 0.0F, 0.0F, 0.1745F);
+		legs.addChild(left_leg_set);
 
-   protected boolean carveBlock(IChunk chunkIn, BitSet carvingMask, Random rand, BlockPos.MutableBlockPos p_222703_4_, BlockPos.MutableBlockPos p_222703_5_, BlockPos.MutableBlockPos p_222703_6_, int p_222703_7_, int p_222703_8_, int p_222703_9_, int p_222703_10_, int p_222703_11_, int p_222703_12_, int p_222703_13_, int p_222703_14_, AtomicBoolean p_222703_15_) {
-      int i = p_222703_12_ | p_222703_14_ << 4 | p_222703_13_ << 8;
-      if (carvingMask.get(i)) {
-         return false;
-      } else {
-         carvingMask.set(i);
-         p_222703_4_.setPos(p_222703_10_, p_222703_13_, p_222703_11_);
-         BlockState blockstate = chunkIn.getBlockState(p_222703_4_);
-         BlockState blockstate1 = chunkIn.getBlockState(p_222703_5_.setPos(p_222703_4_).move(Direction.UP));
-         if (blockstate.getBlock() == Blocks.GRASS_BLOCK || blockstate.getBlock() == Blocks.MYCELIUM) {
-            p_222703_15_.set(true);
-         }
+		left_leg_1 = new RendererModel(this);
+		left_leg_1.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(left_leg_1, -0.5236F, -2.7053F, 0.5236F);
+		left_leg_set.addChild(left_leg_1);
+		left_leg_1.cubeList.add(new ModelBox(left_leg_1, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-         if (!this.canCarveBlock(blockstate, blockstate1)) {
-            return false;
-         } else {
-            if (p_222703_13_ < 11) {
-               chunkIn.setBlockState(p_222703_4_, LAVA.getBlockState(), false);
-            } else {
-               chunkIn.setBlockState(p_222703_4_, CAVE_AIR, false);
-               if (p_222703_15_.get()) {
-                  p_222703_6_.setPos(p_222703_4_).move(Direction.DOWN);
-                  if (chunkIn.getBlockState(p_222703_6_).getBlock() == Blocks.DIRT) {
-                     chunkIn.setBlockState(p_222703_6_, chunkIn.getBiome(p_222703_4_).getSurfaceBuilderConfig().getTop(), false);
-                  }
-               }
-            }
+		left_leg_2 = new RendererModel(this);
+		left_leg_2.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(left_leg_2, -0.5236F, 3.1416F, 0.5236F);
+		left_leg_set.addChild(left_leg_2);
+		left_leg_2.cubeList.add(new ModelBox(left_leg_2, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-            return true;
-         }
-      }
-   }
+		left_leg_3 = new RendererModel(this);
+		left_leg_3.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(left_leg_3, -0.5236F, 2.7053F, 0.5236F);
+		left_leg_set.addChild(left_leg_3);
+		left_leg_3.cubeList.add(new ModelBox(left_leg_3, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-   public abstract boolean carve(IChunk chunkIn, Random rand, int seaLevel, int chunkX, int chunkZ, int p_212867_6_, int p_212867_7_, BitSet carvingMask, C config);
+		right_leg_set = new RendererModel(this);
+		right_leg_set.setRotationPoint(0.0F, -2.0F, 0.0F);
+//		setRotationAngles(right_legs, 0.0F, 0.0F, -0.1745F);
+		legs.addChild(right_leg_set);
 
-   public abstract boolean shouldCarve(Random rand, int chunkX, int chunkZ, C config);
+		right_leg_1 = new RendererModel(this);
+		right_leg_1.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(right_leg_1, 0.5236F, -0.5236F, -0.5236F);
+		right_leg_set.addChild(right_leg_1);
+		right_leg_1.cubeList.add(new ModelBox(right_leg_1, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-   protected boolean func_222706_a(BlockState p_222706_1_) {
-      return this.carvableBlocks.contains(p_222706_1_.getBlock());
-   }
+		right_leg_2 = new RendererModel(this);
+		right_leg_2.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(right_leg_2, 0.5236F, 0.0F, -0.5236F);
+		right_leg_set.addChild(right_leg_2);
+		right_leg_2.cubeList.add(new ModelBox(right_leg_2, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-   protected boolean canCarveBlock(BlockState state, BlockState aboveState) {
-      Block block = state.getBlock();
-      return this.func_222706_a(state) || (block == Blocks.SAND || block == Blocks.GRAVEL) && !aboveState.getFluidState().isTagged(FluidTags.WATER);
-   }
+		right_leg_3 = new RendererModel(this);
+		right_leg_3.setRotationPoint(0.0F, 0.0F, 0.0F);
+//		setRotationAngles(right_leg_3, 0.5236F, 0.5236F, -0.5236F);
+		right_leg_set.addChild(right_leg_3);
+		right_leg_3.cubeList.add(new ModelBox(right_leg_3, 20, 8, -4.0F, -0.5F, -0.5F, 4, 0, 1, 0.0F, false));
 
-   protected boolean func_222700_a(IChunk chunkIn, int chunkX, int chunkZ, int minX, int maxX, int minY, int maxY, int minZ, int maxZ) {
-      BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+//		item = new RendererModel(this);
+//		item.setRotationPoint(0.0F, -6.0F, 4.0F);
+//		setRotationAngle(item, -0.2618F, 0.0F, 0.0F);
+//		body.addChild(item);
+//		item.cubeList.add(new ModelBox(item, 0, 0, -4.0F, 0.5F, -3.5F, 8, 0, 8, 0.0F, false));
+	}
 
-      for(int i = minX; i < maxX; ++i) {
-         for(int j = minZ; j < maxZ; ++j) {
-            for(int k = minY - 1; k <= maxY + 1; ++k) {
-               if (this.carvableFluids.contains(chunkIn.getFluidState(blockpos$mutableblockpos.setPos(i + chunkX * 16, k, j + chunkZ * 16)).getFluid())) {
-                  return true;
-               }
+	public void render(GathererAntEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		this.setRotationAngles(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+		body.render(scale);
+	}
 
-               if (k != maxY + 1 && !this.isOnEdge(minX, maxX, minZ, maxZ, i, j)) {
-                  k = maxY;
-               }
-            }
-         }
-      }
+	public void setRotationAngles(GathererAntEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
 
-      return false;
-   }
 
-   private boolean isOnEdge(int minX, int maxX, int minZ, int maxZ, int x, int z) {
-      return x == minX || x == maxX - 1 || z == minZ || z == maxZ - 1;
-   }
 
-   protected boolean func_222702_a(int p_222702_1_, int p_222702_2_, double p_222702_3_, double p_222702_5_, int p_222702_7_, int p_222702_8_, float p_222702_9_) {
-      double d0 = (double)(p_222702_1_ * 16 + 8);
-      double d1 = (double)(p_222702_2_ * 16 + 8);
-      double d2 = p_222702_3_ - d0;
-      double d3 = p_222702_5_ - d1;
-      double d4 = (double)(p_222702_8_ - p_222702_7_);
-      double d5 = (double)(p_222702_9_ + 2.0F + 16.0F);
-      return d2 * d2 + d3 * d3 - d4 * d4 <= d5 * d5;
-   }
+		head.rotateAngleX = headPitch * (((float) Math.PI) / 180.0F);
+		head.rotateAngleY = netHeadYaw * (((float) Math.PI) / 180.0F);
 
-   protected abstract boolean func_222708_a(double p_222708_1_, double p_222708_3_, double p_222708_5_, int p_222708_7_);
+		setRotationAngles(antennae, 0.5236F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.0F, (MathHelper.sin((limbSwing * 4.00F)) * (0.125F)) + 0.0F);
+		setRotationAngles(abdomen, -0.1745F, 0.0F, 0.0F);
+
+
+		setRotationAngles(left_leg_set, 0.0F, 0.0F, 0.0F);
+
+		setRotationAngles(left_leg_1, -0.0F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 2.6180F, (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+		setRotationAngles(left_leg_2, -0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 3.1416F, (MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+		setRotationAngles(left_leg_3, -0.0F,  MathHelper.sin((limbSwing * 4.00F)) * (0.125F) + 2.6180F, (MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+
+		setRotationAngles(right_leg_set, 0.0F, 0.0F, -0.0F);
+
+		setRotationAngles(right_leg_1, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.5236F, (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
+		setRotationAngles(right_leg_2, 0.0F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.0F   , (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
+		setRotationAngles(right_leg_3, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) + 0.5236F, (MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
+
+
+	}
+
+
+
+	public void setRotationAngles(RendererModel rendererModel, float x, float y, float z) {
+		rendererModel.rotateAngleX = x;
+		rendererModel.rotateAngleY = y;
+		rendererModel.rotateAngleZ = z;
+	}
+
+//	> body
+//    > head
+//        > antennae
+//    > thorax
+//    > legs
+//        > right_legs
+//            > right_leg_1
+//            > right_leg_2
+//            > right_leg_3
+//        > left_legs
+//            > left_leg_1
+//            > left_leg_2
+//            > left_leg_3
+//    > abdomen
+
 }
